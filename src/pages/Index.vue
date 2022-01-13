fa1dbf71aeff775295476f0e3e541b3544a44236
SinusP-CW90 <73338947+SinusP-CW90@users.noreply.github.com>
2022-01-13 23:28:34 +0100

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex">
+    <h4>Github action for deploying Quasar app to GitHub pages</h4>
+    <dev class="text-body1">
+      See the
+      <a href="https://github.com/analythium/github-pages-quasar"
+        >analythium/github-pages-quasar</a
+      >
+      GitHub repository.
+    </dev>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+};
+</script>
